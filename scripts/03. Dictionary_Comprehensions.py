25240d2971e80aece6bd23fe6f00baeeb1f28ac3
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-05-23 09:39:22 -0500

    Dictionary Comprehensions

--- scripts/03. Dictionary_Comprehensions.py
+++ scripts/03. Dictionary_Comprehensions.py
@@ -1,8 +1,13 @@
+import numpy as np
+
 def run():
     # Key: Valor por Valor en Iterable if Condicion
     my_dict = {i: i**3 for i in range(1, 101) if i%3 != 0}
-
     print(my_dict)
+
+    challenge = {i: round(np.sqrt(i), 2) for i in range(1, 1001)}
+    print(challenge)
+
 
 if __name__ == '__main__':
     run()
